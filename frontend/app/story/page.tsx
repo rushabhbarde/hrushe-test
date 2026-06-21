d6176f90ec0a0a1586baa5559cc452e285a5bc4e
rushabhbarde <rushabhbarde@gmail.com>
2026-06-22 02:01:30 +0530

    Bypass stale cache for story page

--- frontend/app/story/page.tsx
+++ frontend/app/story/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 
+export const dynamic = "force-dynamic";
+
 const founders = [
   {
     name: "Hrushabh Barde",
