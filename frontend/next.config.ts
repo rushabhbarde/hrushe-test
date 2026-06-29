df4003fddb2ab0944e358400072a83948764457f
rushabhbarde <rushabhbarde@gmail.com>
2026-06-29 13:44:25 +0530

    Redirect homepage to men

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -33,7 +33,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/",
-        destination: "/women",
+        destination: "/men",
         permanent: false,
       },
       {
